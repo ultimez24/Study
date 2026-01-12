bfd4b30e2b67e27f8673ff67a012b08a473bbbbc
ultimez24 <106946754+ultimez24@users.noreply.github.com>
2026-01-12 15:53:06 +0530

    Create numberfun.php
    Signed-off-by: ultimez24 <106946754+ultimez24@users.noreply.github.com>

---
+++ numberfun.php
@@ -0,0 +1,14 @@
+<?php
+// // number function celi 
+// $pi=443.3432;
+// echo ceil($pi);
+
+// floor
+$p1=7543.829;
+echo floor($p1);
+
+// //random
+// $randNumber = rand(500 , 5000);
+// echo $randNumber;
+
+?>
